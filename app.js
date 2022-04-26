da2c3f8ab67d985d59d3220b3785d3eb6fb2e427
Bobby <bobby.jonkman@hotmail.com>
2022-04-26 13:29:37 -0400

    created table shell, ready for connection to mysql db

--- app.js
+++ app.js
@@ -1,44 +1,64 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+// include config file.
+const config = require("./config/config");
+
+// include mysql.
+let mysql = require("mysql");
 
 // create routes here.
-var indexRouter = require('./routes/index');
+var indexRouter = require("./routes/index");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // use routes here.
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // create connection to database.
+let connection = mysql.createConnection({
+  host: config.host,
+  user: config.user,
+  password: config.password,
+  database: config.database,
+});
 
+// connect to db.
+connection.connect(function (err) {
+  if (err) {
+    return console.error("error: " + err.message);
+  }
+  // print success message to the console.
+  console.log("Connected to: testdb");
+});
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
