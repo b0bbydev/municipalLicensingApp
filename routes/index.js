109803ea12c0f4f8f327d74cb9444fad964e5152
Bobby <bobby.jonkman@hotmail.com>
2022-04-26 15:35:15 -0400

    adding a new value to dropdown menu working

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get("/", function (req, res, next) {
 
 /* DELETE dropdown value */
 router.get("/delete/:id", (req, res, next) => {
-  // *SHOULD VALIDATE THIS*
+  // *NEED TO VALIDATE THIS*
   var dropdownID = req.params.id;
 
   // create the query.
@@ -32,6 +32,23 @@ router.get("/delete/:id", (req, res, next) => {
 
   // redirect to home after success.
   res.redirect("/");
+});
+
+/* POST dropdown value */
+router.post("/", (req, res, next) => {
+  // get data from form
+  var value = req.body.value
+
+  // create the SQL query.
+  var query = "INSERT INTO dropdown (value) VALUES (?)";
+
+  // call query on db.
+  db.query(query, [value], function (err, data) {
+    if(err) {
+      console.log(err);
+    }
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
