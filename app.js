c7dc7c13e10917dc7e5729a6c67107d05f12af3c
Bobby <bobby.jonkman@hotmail.com>
2022-04-26 12:46:18 -0400

    added mysql package and index.hbs setup

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+// create connection to database.
+
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
