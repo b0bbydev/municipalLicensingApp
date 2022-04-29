4562072654834c231ff208e8c20c82ba0a6b0016
Bobby <bobby.jonkman@hotmail.com>
2022-04-29 14:10:28 -0400

    logout functional

--- routes/index.js
+++ routes/index.js
@@ -129,4 +129,10 @@ router.post("/login", (req, res) => {
   });
 });
 
+/* GET logout page */
+router.get("/logout", function (req, res, next) {
+  req.session.destroy();
+  res.redirect("/login");
+});
+
 module.exports = router;
