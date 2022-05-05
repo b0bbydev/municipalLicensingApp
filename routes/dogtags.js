622671a6f0e5fd8f3dae8f9cb42fbd983228406b
Bobby <bobby.jonkman@hotmail.com>
2022-05-05 14:13:22 -0400

    owners data is displayed in table

--- routes/dogtags.js
+++ routes/dogtags.js
@@ -1,11 +1,25 @@
 var express = require("express");
 var router = express.Router();
+// db config.
+var db = require("../config/db");
+// dbHelpers.
+var dbHelpers = require("../config/dbHelpers");
 
 /* GET login page. */
 router.get("/", function (req, res, next) {
-  res.render("dogtags", {
-    title: "BWG | Dog Tags",
-    email: req.session.email,
+  // create mySQL query.
+  var query = "SELECT * FROM owners LIMIT 10";
+
+  db.query(query, function (err, data) {
+    if (err) {
+      console.log("Error: ", err);
+    }
+
+    res.render("dogtags", {
+      title: "BWG | Dog Tags",
+      email: req.session.email,
+      data: data,
+    });
   });
 });
 
