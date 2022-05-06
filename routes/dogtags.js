04cdfe0717640527e819048c0c8200eb536ae1a9
Bobby <bobby.jonkman@hotmail.com>
2022-05-06 11:16:02 -0400

    pagination still scuffed and not working

--- routes/dogtags.js
+++ routes/dogtags.js
@@ -2,18 +2,16 @@ var express = require("express");
 var router = express.Router();
 // db config.
 var db = require("../config/db");
+// dbHelpers.
+var dbHelpers = require("../config/dbHelpers");
 
-/* GET login page. */
-router.get("/", function (req, res, next) {
-  // create mySQL query.
-  var query =
-    "SELECT * FROM owners LEFT JOIN dogs ON owners.ownerID = dogs.ownerID LIMIT 10";
-
+/* GET dogtag page. */
+router.get("/", async (req, res, next) => {
   // create pagination query.
   var pagQuery =
     "SELECT * FROM owners LEFT JOIN dogs ON owners.ownerID = dogs.ownerID LIMIT 10 OFFSET ?";
 
-  db.query(pagQuery, parseInt([req.query.offset || 0]), function (err, data) {
+  db.query(pagQuery, [parseInt(req.query.offset) || 0], function (err, data) {
     if (err) {
       console.log("Error: ", err);
     }
