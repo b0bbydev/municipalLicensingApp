aca43700af0af5d50f056151ed15ce551448620e
Bobby <bobby.jonkman@hotmail.com>
2022-05-09 10:56:08 -0400

    added enable functionality to dropdown values

--- routes/index.js
+++ routes/index.js
@@ -64,6 +64,31 @@ router.get("/dropdown/disable/:id", (req, res, next) => {
   }
 });
 
+/* DISABLE dropdown value */
+router.get("/dropdown/enable/:id", (req, res, next) => {
+  // validate to make sure only a number can be passed here.
+  if (!req.params.id.match(/^\d+$/)) {
+    // if something other than a number is passed, redirect again to dropdown.
+    res.redirect("/dropdown");
+  } else {
+    // save dropdownID.
+    var dropdownID = req.params.id;
+
+    // create the query.
+    var query = "UPDATE dropdown SET isDisabled = 0 WHERE dropdownID = ?";
+
+    // call query on db.
+    db.query(query, [dropdownID], function (err, data) {
+      if (err) {
+        console.log(err);
+      }
+    });
+
+    // redirect to home after success.
+    res.redirect("/dropdown");
+  }
+});
+
 /* GET logout page */
 router.get("/logout", function (req, res, next) {
   // destory the session.
