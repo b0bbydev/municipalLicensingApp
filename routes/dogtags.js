5bb73cfeb1fe208d671618d7f73e8d923fc00212
Bobby <bobby.jonkman@hotmail.com>
2022-05-09 13:54:40 -0400

    switching to implementing filtering

--- routes/dogtags.js
+++ routes/dogtags.js
@@ -25,8 +25,9 @@ router.get("/", async (req, res, next) => {
       isAdmin: req.session.isAdmin,
       email: req.session.email,
       data: data,
+      hasPrev: paginate.hasPreviousPages,
       hasNext: paginate.hasNextPages(req)(req.query.page),
-      pages: paginate.getArrayPages(req)(pageCount - 1, pageCount, req.query.page),
+      pages: paginate.getArrayPages(req)(pageCount, pageCount, req.query.page),
     });
   });
 });
