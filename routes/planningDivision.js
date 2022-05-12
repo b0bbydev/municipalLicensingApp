fff0920e83d08d22f6be66b5e7cfc563925cf0c3
Bobby <bobby.jonkman@hotmail.com>
2022-05-12 15:37:52 -0400

    included admin options in planningDivision route

--- routes/planningDivision.js
+++ routes/planningDivision.js
@@ -5,6 +5,8 @@ var router = express.Router();
 router.get("/", function (req, res, next) {
   res.render("planningDivision", {
     title: "BWG | Planning Division",
+    isAdmin: req.session.isAdmin,
+    email: req.session.email,
   });
 });
 
