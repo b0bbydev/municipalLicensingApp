15a6a3fde80105cca872ae2a59474d41a8864a19
Bobby <bobby.jonkman@hotmail.com>
2022-05-13 11:19:43 -0400

    added client-side and server-side validation to login password

--- routes/login.js
+++ routes/login.js
@@ -25,51 +25,59 @@ router.get("/", function (req, res, next) {
 });
 
 /* POST for /login */ /* note the middleware from express-validate. */
-router.post("/", body("email").isEmail().normalizeEmail(), (req, res) => {
-  // server side validation.
-  const errors = validationResult(req);
+router.post(
+  "/",
+  body("email").isEmail().normalizeEmail().trim(),
+  body("password")
+    // Minimum 12 characters; at least one uppercase letter, one lowercase letter, one special character
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$/)
+    .trim(),
+  (req, res) => {
+    // server side validation.
+    const errors = validationResult(req);
 
-  // if errors is NOT empty (if there are errors...)
-  if (!errors.isEmpty()) {
-    // render login page with error message.
-    return res.render("login", {
-      title: "BWG",
-      message: "Invalid email/password combination!",
-      layout: "hideLayout.hbs",
-    });
-  } else {
-    // assuming the email is valid (will have passed client and server side validation at this point).
-    // get the values from the login form.
-    const email = req.body.email;
-    const password = req.body.password;
+    // if errors is NOT empty (if there are errors...)
+    if (!errors.isEmpty()) {
+      // render login page with error message.
+      return res.render("login", {
+        title: "BWG",
+        message: "Invalid email and/or password!",
+        layout: "hideLayout.hbs",
+      });
+    } else {
+      // assuming the email is valid (will have passed client and server side validation at this point).
+      // get the values from the login form.
+      const email = req.body.email;
+      const password = req.body.password;
 
-    ad.authenticate(email, password, function (err, auth) {
-      // if there's an error with AD.
-      if (err) {
-        console.log("ERROR: " + JSON.stringify(err));
+      ad.authenticate(email, password, function (err, auth) {
+        // if there's an error with AD.
+        if (err) {
+          console.log("ERROR: " + JSON.stringify(err));
 
-        // render login page with error message.
-        return res.render("login", {
-          title: "BWG",
-          message: "Login Error!",
-          layout: "hideLayout.hbs",
-        });
-      }
-      // if login is successful.
-      if (auth) {
-        // set the email for the session.
-        req.session.email = email;
-        res.redirect("/");
-      } else {
-        // render login page with error message.
-        return res.render("login", {
-          title: "BWG",
-          message: "Login Error!",
-          layout: "hideLayout.hbs",
-        });
-      }
-    });
+          // render login page with error message.
+          return res.render("login", {
+            title: "BWG",
+            message: "Login Error!",
+            layout: "hideLayout.hbs",
+          });
+        }
+        // if login is successful.
+        if (auth) {
+          // set the email for the session.
+          req.session.email = email;
+          res.redirect("/");
+        } else {
+          // render login page with error message.
+          return res.render("login", {
+            title: "BWG",
+            message: "Login Error!",
+            layout: "hideLayout.hbs",
+          });
+        }
+      });
+    }
   }
-});
+);
 
 module.exports = router;
