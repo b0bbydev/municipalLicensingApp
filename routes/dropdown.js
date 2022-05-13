c4d2f8bb8d8903168faec71f25b5d8004749f2a0
Bobby <bobby.jonkman@hotmail.com>
2022-05-13 13:14:54 -0400

    converted dropdown queries to async functions

--- routes/dropdown.js
+++ routes/dropdown.js
@@ -1,32 +1,26 @@
 var express = require("express");
 var router = express.Router();
 const { redirectToLogin } = require("../config/authHelpers");
-// db config.
-var db = require("../config/db");
+// dbHelpers.
+var dbHelpers = require("../config/dbHelpers");
 // express-validate.
 const { body, validationResult } = require("express-validator");
 
 /* GET dropdown page. */
-router.get("/", function (req, res, next) {
+router.get("/", async (req, res, next) => {
   // check if there's an error message in the session
   let messages = req.session.messages || [];
 
   // clear session messages
   req.session.messages = [];
 
-  // create mySQL query.
-  var query = "SELECT * FROM dropdown";
+  var data = await dbHelpers.getAllFromDropdown();
 
-  db.query(query, function (err, data) {
-    if (err) {
-      console.log("Error: ", err);
-    }
-    res.render("dropdown", {
-      title: "BWG | Dropdown",
-      errorMessages: messages,
-      email: req.session.email,
-      data: data,
-    });
+  return res.render("dropdown", {
+    title: "BWG | Dropdown",
+    errorMessages: messages,
+    email: req.session.email,
+    data: data,
   });
 });
 
@@ -48,19 +42,8 @@ router.post(
         message: "Invalid entry!",
       });
     } else {
-      // get data from form
-      var value = req.body.value;
-
-      // create the SQL query.
-      var query = "INSERT INTO dropdown (value) VALUES (?)";
-
-      // call query on db.
-      db.query(query, [value], function (err, data) {
-        // do something on error.
-        if (err) {
-          console.log(err);
-        }
-      });
+      // insert into db.
+      dbHelpers.insertToDropdown(req.body.value);
 
       // redirect to /dropdown if successful. (reload page)
       res.redirect("/dropdown");
