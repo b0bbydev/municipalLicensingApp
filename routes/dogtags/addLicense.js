361d82a7af9c5da555295b533da91341dd75cef7
Bobby <bobby.jonkman@hotmail.com>
2022-05-17 15:59:42 -0400

    adjusted footer CSS to match height of logout in side-nav

--- routes/dogtags/addLicense.js
+++ routes/dogtags/addLicense.js
@@ -24,14 +24,24 @@ router.post("/", async (req, res, next) => {
   dbHelpers.insertOwnerAndDogInfo(
     req.body.firstName,
     req.body.lastName,
+    req.body.poBoxAptRR,
+    req.body.address,
+    req.body.town,
+    req.body.postalCode,
     req.body.homePhone,
     req.body.cellPhone,
     req.body.workPhone,
     req.body.email,
-    req.body.address,
-    req.body.poBoxAptRR,
-    req.body.town,
-    req.body.postalCode
+    req.body.tagNumber,
+    req.body.dogName,
+    req.body.breed,
+    req.body.colour,
+    req.body.dateOfBirth,
+    req.body.gender,
+    req.body.spade,
+    req.body.designation,
+    req.body.rabiesTagNumber,
+    req.body.rabiesExpiry
   );
 
   // redirect back to dogtag index after success.
