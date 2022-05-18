f90efc374c6ce0729034c321204c736bd421878b
Bobby <bobby.jonkman@hotmail.com>
2022-05-18 15:32:34 -0400

    license renew routing and view/form setup

--- routes/dogtags/index.js
+++ routes/dogtags/index.js
@@ -168,4 +168,19 @@ router.post("/addDog/:id", async (req, res, next) => {
   res.redirect("/dogtags");
 });
 
+/* GET /dogtags/renew/:id page. */
+router.get("/renew/:id", async (req, res, next) => {
+  // check if there's an error message in the session
+  let messages = req.session.messages || [];
+
+  // clear session messages
+  req.session.messages = [];
+
+  res.render("dogtags/renew", {
+    title: "BWG | Owner",
+    errorMessages: messages,
+    email: req.session.email,
+  });
+});
+
 module.exports = router;
