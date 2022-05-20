689235819ba0e7383ffbfd0d02d1439cef07c62d
Bobby <bobby.jonkman@hotmail.com>
2022-05-20 11:27:19 -0400

    retained form values if invalid submission

--- routes/dogtags/addOwner.js
+++ routes/dogtags/addOwner.js
@@ -25,9 +25,9 @@ router.post(
   "/",
   body("firstName").isAlpha().trim(),
   body("lastName").isAlpha().trim(),
-  body("homePhone").isMobilePhone().trim(),
-  body("cellPhone").isMobilePhone().trim(),
-  body("workPhone").isMobilePhone().trim(),
+  body("homePhone").if(body("homePhone").notEmpty()).isMobilePhone().trim(),
+  body("cellPhone").if(body("cellPhone").notEmpty()).isMobilePhone().trim(),
+  body("workPhone").if(body("workPhone").notEmpty()).isMobilePhone().trim(),
   body("email").isEmail().trim(),
   body("address").isAlphanumeric().trim(),
   body("poBoxAptRR").isNumeric().trim(),
@@ -42,6 +42,18 @@ router.post(
       return res.render("dogtags/addOwner", {
         title: "BWG | Owner",
         message: "Form Error!",
+        formData: {
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          homePhone: req.body.homePhone,
+          cellPhone: req.body.cellPhone,
+          workPhone: req.body.workPhone,
+          email: req.body.email,
+          address: req.body.address,
+          poBoxAptRR: req.body.poBoxAptRR,
+          town: req.body.town,
+          postalCode: req.body.postalCode,
+        },
       });
     } else {
       // insert into owner table.
