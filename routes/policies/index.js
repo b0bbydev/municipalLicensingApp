1cc5e1f1b2b0e6cabf619237fbb6b4eaddbcb455
Bobby <bobby.jonkman@hotmail.com>
2022-05-25 16:14:52 -0400

    policies displayed in policy view

--- routes/policies/index.js
+++ routes/policies/index.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var router = express.Router();
+const { redirectToLogin } = require("../../config/authHelpers");
+// dbHelpers.
+var dbHelpers = require("../../config/dbHelpers");
 
 /* GET /policies */
 router.get("/", async (req, res, next) => {
@@ -9,8 +12,8 @@ router.get("/", async (req, res, next) => {
   // clear session messages
   req.session.messages = [];
 
-  // db stuff.
-  var data = "";
+  // get all the policies.
+  var data = await dbHelpers.getAllPolicies();
 
   return res.render("policies/index", {
     title: "BWG | Policies & Procedures",
