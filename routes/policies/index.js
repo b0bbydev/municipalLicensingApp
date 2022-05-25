eb5848ea05eff8e4a4c2e296c4da0505ab39e173
Bobby <bobby.jonkman@hotmail.com>
2022-05-25 16:04:29 -0400

    created initial table on policies page

--- routes/policies/index.js
+++ routes/policies/index.js
@@ -2,10 +2,21 @@ var express = require("express");
 var router = express.Router();
 
 /* GET /policies */
-router.get("/", function (req, res, next) {
-  res.render("policies/index", {
+router.get("/", async (req, res, next) => {
+  // check if there's an error message in the session
+  let messages = req.session.messages || [];
+
+  // clear session messages
+  req.session.messages = [];
+
+  // db stuff.
+  var data = "";
+
+  return res.render("policies/index", {
     title: "BWG | Policies & Procedures",
+    errorMessages: messages,
     email: req.session.email,
+    data: data,
   });
 });
 
