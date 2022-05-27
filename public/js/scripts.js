6d9d9685646d32d70f0cc8c557241ebf1cf2ca4e
Bobby <bobby.jonkman@hotmail.com>
2022-05-27 11:05:53 -0400

    adjusted triggers to properly work

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -15,3 +15,17 @@ function toggleAddressHistory() {
   addressHistorySection.style.display =
     addressHistorySection.style.display === "none" ? "" : "none";
 }
+
+// create function to fill out form.
+function fillForm() {
+  document.getElementById("firstName").value = "Test";
+  document.getElementById("lastName").value = "Test";
+  document.getElementById("poBoxAptRR").value = "Test";
+  document.getElementById("address").value = "Test";
+  document.getElementById("town").value = "Test";
+  document.getElementById("postalCode").value = "Test";
+  document.getElementById("homePhone").value = "123-123-1234";
+  document.getElementById("cellPhone").value = "123-123-1234";
+  document.getElementById("workPhone").value = "123-123-1234";
+  document.getElementById("email").value = "test@test.com";
+}
