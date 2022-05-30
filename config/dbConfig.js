97c3827d8eac6b1534c97fc1563277b25b376dc3
Bobby <bobby.jonkman@hotmail.com>
2022-05-30 09:46:51 -0400

    created owner model & connected to testdb

---
+++ config/dbConfig.js
@@ -0,0 +1,16 @@
+// dotenv.
+require("dotenv").config();
+// sequelize.
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.USER,
+  process.env.PASSWORD,
+  {
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+  }
+);
+
+module.exports = sequelize;
---
+++ models/owner.js
@@ -0,0 +1,50 @@
+// sequelize.
+const Sequelize = require("sequelize");
+
+const sequelize = require("../config/dbConfig");
+
+const Owner = sequelize.define("owner", {
+  ownerID: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+
+  homePhone: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+
+  homePhone: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+
+  cellPhone: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+
+  workPhone: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+
+  email: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+});
+
+module.exports = Owner;
