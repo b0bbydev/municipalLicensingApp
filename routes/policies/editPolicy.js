97a515b34fe11b156298ca7a28bafde42cffed66
Bobby <bobby.jonkman@hotmail.com>
2022-06-09 16:14:23 -0400

    editPolicy POST routing working

--- routes/policies/editPolicy.js
+++ routes/policies/editPolicy.js
@@ -2,10 +2,11 @@ var express = require("express");
 var router = express.Router();
 // models.
 const Dropdown = require("../../models/dropdownManager/dropdown");
+const Policy = require("../../models/policies/policy");
 // dbHelpers.
 var dbHelpers = require("../../config/dbHelpers");
 // express-validate.
-const { param, validationResult } = require("express-validator");
+const { param, body, validationResult } = require("express-validator");
 
 /* GET /policies/editPolicy/:id */
 router.get(
@@ -24,9 +25,11 @@ router.get(
     } else {
       // check if there's an error message in the session.
       let messages = req.session.messages || [];
-
       // clear session messages.
       req.session.messages = [];
+
+      // save policyID to session.
+      req.session.policyID = req.params.id;
 
       // dropdown values.
       // status options.
@@ -75,6 +78,120 @@ router.get(
           notes: policyInfo[0].notes,
         },
       });
+    }
+  }
+);
+
+/* POST /policies/editPolicy/:id */
+router.post(
+  "/:id",
+  body("policyNumber")
+    .if(body("policyNumber").notEmpty())
+    .matches(/^[0-9-]*$/)
+    .withMessage("Invalid Policy Number Entry!")
+    .trim(),
+  body("policyName")
+    .if(body("policyName").notEmpty())
+    .matches(/^[a-zA-z\/\- ]*$/)
+    .withMessage("Invalid Policy Name Entry!")
+    .trim(),
+  body("cowResolve")
+    .if(body("cowResolve").notEmpty())
+    .matches(/^\d{4}-\d{2}-\d{2}$/)
+    .withMessage("Invalid COW Resolve Entry!")
+    .trim(),
+  body("cowDate")
+    .if(body("cowDate").notEmpty())
+    .matches(/^\d{4}-\d{2}-\d{2}$/)
+    .withMessage("Invalid COW Date Entry!")
+    .trim(),
+  body("status")
+    .if(body("status").notEmpty())
+    .matches(/^[a-zA-z\/\- ]*$/)
+    .withMessage("Invalid Status Entry!")
+    .trim(),
+  body("lastReviewDate")
+    .if(body("lastReviewDate").notEmpty())
+    .matches(/^\d{4}-\d{2}-\d{2}$/)
+    .withMessage("Invalid Last Review Date Entry!")
+    .trim(),
+  body("scheduledReviewDate")
+    .if(body("scheduledReviewDate").notEmpty())
+    .matches(/^\d{4}-\d{2}-\d{2}$/)
+    .withMessage("Invalid Scheduled Review Date Entry!")
+    .trim(),
+  body("category")
+    .if(body("category").notEmpty())
+    .matches(/^[a-zA-z\/\- ]*$/)
+    .withMessage("Invalid Category Entry!")
+    .trim(),
+  body("authority")
+    .if(body("authority").notEmpty())
+    .matches(/^[a-zA-z\/\- ]*$/)
+    .withMessage("Invalid Authority Entry!")
+    .trim(),
+  body("notes")
+    .if(body("notes").notEmpty())
+    .matches(/^[a-zA-z0-9\/\-, ]*$/)
+    .withMessage("Invalid Notes Entry!")
+    .trim(),
+  async (req, res, next) => {
+    // server side validation.
+    const errors = validationResult(req);
+
+    // use built-in array() to convert Result object to array for custom error messages.
+    var errorArray = errors.array();
+
+    // if errors is NOT empty (if there are errors...).
+    if (!errors.isEmpty()) {
+      return res.render("policies/addPolicy", {
+        title: "BWG | Add Policy",
+        message: errorArray[0].msg,
+        email: req.session.email,
+        // if the form submission is unsuccessful, save their values.
+        formData: {
+          policyNumber: req.body.policyNumber,
+          policyName: req.body.policyName,
+          cowResolve: req.body.cowResolve,
+          cowDate: req.body.cowDate,
+          councilResolution: req.body.councilResolution,
+          dateAdopted: req.body.dateAdopted,
+          dateAmended: req.body.dateAmended,
+          status: req.body.status,
+          lastReviewDate: req.body.lastReviewDate,
+          scheduledReviewDate: req.body.scheduledReviewDate,
+          category: req.body.category,
+          authority: req.body.authority,
+          notes: req.body.notes,
+        },
+      });
+    } else {
+      // db stuff.
+      Policy.update(
+        {
+          policyNumber: req.body.policyNumber,
+          policyName: req.body.policyName,
+          cowResolve: req.body.cowResolve,
+          cowDate: req.body.cowDate,
+          councilResolution: req.body.councilResolution,
+          dateAdopted: req.body.dateAdopted,
+          dateAmended: req.body.dateAmended,
+          status: req.body.status,
+          lastReviewDate: req.body.lastReviewDate,
+          scheduledReviewDate: req.body.scheduledReviewDate,
+          category: req.body.category,
+          authority: req.body.authority,
+          notes: req.body.notes,
+        },
+        {
+          where: {
+            policyID: req.session.policyID,
+          },
+        }
+      );
+
+      // redirect to /policies
+      res.redirect("/policies");
     }
   }
 );
