0ab1567f28224d3d05474c111437a53bbf974b2d
Bobby <bobby.jonkman@hotmail.com>
2022-06-15 11:09:47 -0400

    test

--- routes/dogtags/addAdditionalOwner.js
+++ routes/dogtags/addAdditionalOwner.js
@@ -6,8 +6,16 @@ const AdditionalOwner = require("../../models/dogtags/additionalOwner");
 const { body, validationResult } = require("express-validator");
 // authHelper middleware.
 const { redirectToLogin } = require("../../config/authHelpers");
-// limiter.
-const limiter = require("../../config/limiter");
+// express-rate-limit.
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  message: "Too many requests! Slow down!",
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 /* GET addAdditionalOwner page. */
 router.get("/", limiter, async (req, res, next) => {
