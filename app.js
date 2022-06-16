f2d1c6f4e863216c5341c8ae5e690ead04541238
Bobby <bobby.jonkman@hotmail.com>
2022-06-16 15:21:22 -0400

    had to get a commit in

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ var indexRouter = require("./routes/index");
 const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 15 * 60 * 1000, // 15 minutes.
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   message: "Too many requests! Slow down!",
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
