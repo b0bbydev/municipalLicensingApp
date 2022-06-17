10759dbb5fda605ed6f2ddb1b7e25727a55bd5c0
Bobby <bobby.jonkman@hotmail.com>
2022-06-17 15:52:09 -0400

    working web.config file

--- config/sequelizeConfig.js
+++ config/sequelizeConfig.js
@@ -5,7 +5,7 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
   process.env.DATABASE,
-  "bobby",
+  "root",
   process.env.PASSWORD,
   {
     host: process.env.HOST,
