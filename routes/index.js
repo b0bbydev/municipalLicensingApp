d05718942774db3eb89ce81061ec9e2c866d85ac
Bobby <bobby.jonkman@hotmail.com>
2022-07-05 16:24:00 -0400

    added auth back

--- routes/index.js
+++ routes/index.js
@@ -10,24 +10,30 @@ const {
 const limiter = require("../config/limiter");
 
 /* GET home page. */
-router.get("/", limiter, function (req, res, next) {
-  // check if there's an error message in the session
-  let messages = req.session.messages || [];
-  // clear session messages
-  req.session.messages = [];
-  // delete session lastEnteredDropdownTitle.
-  delete req.session.lastEnteredDropdownTitle;
+router.get(
+  "/",
+  isLoggedIn,
+  adminAuth,
+  dogLicenseAuth,
+  function (req, res, next) {
+    // check if there's an error message in the session
+    let messages = req.session.messages || [];
+    // clear session messages
+    req.session.messages = [];
+    // delete session lastEnteredDropdownTitle.
+    delete req.session.lastEnteredDropdownTitle;
 
-  return res.render("index", {
-    title: "BWG | Home",
-    errorMessages: messages,
-    email: req.session.email,
-    dogAuth: req.session.dogAuth, // authorization.
-    admin: req.session.admin, // authorization.
-    baseUrl: req.baseUrl, // back button logic.
-    indexUrl: "/", // back button logic.
-  });
-});
+    return res.render("index", {
+      title: "BWG | Home",
+      errorMessages: messages,
+      email: req.session.email,
+      dogAuth: req.session.dogAuth, // authorization.
+      admin: req.session.admin, // authorization.
+      baseUrl: req.baseUrl, // back button logic.
+      indexUrl: "/", // back button logic.
+    });
+  }
+);
 
 /* GET logout page */
 router.get("/logout", function (req, res, next) {
