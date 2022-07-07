77b1912c48e66236acd0edbbe2728dad45b5f8a4
Bobby <bobby.jonkman@hotmail.com>
2022-07-07 15:08:46 -0400

    created user model

---
+++ models/admin/user.js
@@ -0,0 +1,30 @@
+// sequelize.
+const Sequelize = require("sequelize");
+// db.
+const sequelize = require("../../config/sequelizeConfig");
+
+const User = sequelize.define("user", {
+  userID: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+
+  firstName: {
+    type: Sequelize.STRING(25),
+    allowNull: true,
+  },
+
+  lastName: {
+    type: Sequelize.STRING(25),
+    allowNull: true,
+  },
+
+  email: {
+    type: Sequelize.STRING(50),
+    allowNull: true,
+  },
+});
+
+module.exports = User;
