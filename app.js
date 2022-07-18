e79d7f95150f02755b115bb329430944ec19bf80
Bobby <bobby.jonkman@hotmail.com>
2022-07-18 13:00:42 -0400

    procedure & guideline view/routing setup

--- app.js
+++ app.js
@@ -49,9 +49,11 @@ var addRecordRouter = require("./routes/policies/addRecord");
 var addPoliciesRouter = require("./routes/policies/addPolicy");
 var editPolicyRouter = require("./routes/policies/editPolicy");
 
+var proceduresRouter = require("./routes/policies/procedure");
 var addProcedureRouter = require("./routes/policies/addProcedure");
 var editProcedureRouter = require("./routes/policies/editProcedure");
 
+var guidelinesRouter = require("./routes/policies/guideline");
 var addGuidelineRouter = require("./routes/policies/addGuideline");
 var editGuidelineRouter = require("./routes/policies/editGuideline");
 
@@ -208,9 +210,11 @@ app.use("/policies/addRecord", isPolicy, addRecordRouter);
 app.use("/policies/addPolicy", isPolicy, addPoliciesRouter);
 app.use("/policies/editPolicy", isPolicy, editPolicyRouter);
 
+app.use("/policies/procedure", isPolicy, proceduresRouter);
 app.use("/policies/addProcedure", isPolicy, addProcedureRouter);
 app.use("/policies/editProcedure", isPolicy, editProcedureRouter);
 
+app.use("/policies/guideline", isPolicy, guidelinesRouter);
 app.use("/policies/addGuideline", isPolicy, addGuidelineRouter);
 app.use("/policies/editGuideline", isPolicy, editGuidelineRouter);
 
---
+++ routes/policies/procedure.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET /policies/procedure page. */
+router.get("/", async (req, res, next) => {
+  // check if there's an error message in the session
+  let messages = req.session.messages || [];
+  // clear session messages
+  req.session.messages = [];
+
+  return res.render("policies/procedure", {
+    title: "BWG | Procedures",
+    errorMessages: messages,
+    email: req.session.email,
+    auth: req.session.auth, // authorization.
+  });
+});
+
+module.exports = router;
