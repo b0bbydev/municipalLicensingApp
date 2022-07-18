77b211910bd481498717742e3503b23cc265aca1
Bobby <bobby.jonkman@hotmail.com>
2022-07-18 14:41:35 -0400

    able to search unrelated procedures and guidelines by name

--- routes/policies/procedures.js
+++ routes/policies/procedures.js
@@ -6,6 +6,8 @@ const Procedure = require("../../models/policies/procedure");
 // sequelize.
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
+// helper.
+const funcHelpers = require("../../config/funcHelpers");
 // pagination lib.
 const paginate = require("express-paginate");
 
@@ -23,34 +25,85 @@ router.get("/", async (req, res, next) => {
     },
   });
 
-  Procedure.findAndCountAll({
-    limit: req.query.limit,
-    offset: req.skip,
-    where: {
-      policyID: {
-        [Op.eq]: null, // where policyID is null.
+  // if there are no filter parameters.
+  if (!req.query.filterCategory || !req.query.filterValue) {
+    Procedure.findAndCountAll({
+      limit: req.query.limit,
+      offset: req.skip,
+      where: {
+        policyID: {
+          [Op.eq]: null, // where policyID is null.
+        },
       },
-    },
-  }).then((results) => {
-    // for pagination.
-    const itemCount = results.count;
-    const pageCount = Math.ceil(results.count / req.query.limit);
+    }).then((results) => {
+      // for pagination.
+      const itemCount = results.count;
+      const pageCount = Math.ceil(results.count / req.query.limit);
 
-    return res.render("policies/procedures", {
-      title: "BWG | Procedures",
-      errorMessages: messages,
-      email: req.session.email,
-      auth: req.session.auth, // authorization.
-      data: results.rows,
-      dropdownValues: dropdownValues,
-      pageCount,
-      itemCount,
-      queryCount: "Records returned: " + results.count,
-      pages: paginate.getArrayPages(req)(5, pageCount, req.query.page),
-      prev: paginate.href(req)(true),
-      hasMorePages: paginate.hasNextPages(req)(pageCount),
+      return res.render("policies/procedures", {
+        title: "BWG | Procedures",
+        errorMessages: messages,
+        email: req.session.email,
+        auth: req.session.auth, // authorization.
+        data: results.rows,
+        dropdownValues: dropdownValues,
+        pageCount,
+        itemCount,
+        queryCount: "Records returned: " + results.count,
+        pages: paginate.getArrayPages(req)(5, pageCount, req.query.page),
+        prev: paginate.href(req)(true),
+        hasMorePages: paginate.hasNextPages(req)(pageCount),
+      });
     });
-  });
+  } else {
+    // format filterCategory to match column name in db - via handy dandy camelize() function.
+    var filterCategory = funcHelpers.camelize(req.query.filterCategory);
+
+    // create filter query.
+    Procedure.findAndCountAll({
+      where: {
+        [filterCategory]: {
+          [Op.like]: "%" + req.query.filterValue + "%",
+        },
+        [Op.and]: {
+          policyID: {
+            [Op.eq]: null, // where policyID is null.
+          },
+        },
+      },
+      limit: req.query.limit,
+      offset: req.skip,
+    })
+      .then((results) => {
+        // for pagination.
+        const itemCount = results.count;
+        const pageCount = Math.ceil(results.count / req.query.limit);
+
+        return res.render("policies/procedures", {
+          title: "BWG | Procedures",
+          errorMessages: messages,
+          email: req.session.email,
+          auth: req.session.auth, // authorization.
+          data: results.rows,
+          filterCategory: req.query.filterCategory,
+          filterValue: req.query.filterValue,
+          dropdownValues: dropdownValues,
+          pageCount,
+          itemCount,
+          queryCount: "Records returned: " + results.count,
+          pages: paginate.getArrayPages(req)(5, pageCount, req.query.page),
+          prev: paginate.href(req)(true),
+          hasMorePages: paginate.hasNextPages(req)(pageCount),
+        });
+      })
+      // catch any scary errors and render page error.
+      .catch((err) =>
+        res.render("policies/procedures", {
+          title: "BWG | Procedures",
+          message: "Page Error!",
+        })
+      );
+  }
 });
 
 module.exports = router;
--- routes/sendSms/index.js
+++ routes/sendSms/index.js
@@ -41,8 +41,8 @@ router.post("/", function (req, res, next) {
   let client = new plivo.Client(process.env.AUTH_ID, process.env.AUTH_TOKEN);
   client.messages
     .create({
-      src: "TownOfBWG",
-      dst: "+19057156835",
+      src: "+19057156835", // me
+      dst: "+12893835612", // chris
       text: req.body.textMessage,
     })
     .then(function (message_created) {
