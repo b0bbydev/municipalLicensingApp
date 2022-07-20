5c3994becd7927ccd49284d89504efcfecd2f27c
Bobby <bobby.jonkman@hotmail.com>
2022-07-20 11:13:42 -0400

    cleanup

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -34,58 +34,6 @@ function toggleGuidelineHistory() {
     guidelineHistorySection.style.display === "none" ? "" : "none";
 }
 
-/* auth helpers */
-function adminAuth(modalID) {
-  // get current text in input field to append.
-  var currentText = document.getElementById(
-    "currentAuthLevel-" + modalID
-  ).value;
-
-  // check if currentText already contains 'Admin'. If it does, don't add it (return).
-  if (currentText.includes("Admin")) {
-    document.getElementById("currentAuthLevel-" + modalID).value =
-      currentText.replace("Admin,", "");
-  } else {
-    // add Admin to list.
-    document.getElementById("currentAuthLevel-" + modalID).value =
-      currentText + "Admin, ";
-  }
-}
-
-function dogLicenseAuth(modalID) {
-  // get current text in input field to append.
-  var currentText = document.getElementById(
-    "currentAuthLevel-" + modalID
-  ).value;
-
-  // check if currentText already contains 'Dog Licenses'. If it does, don't add it (return).
-  if (currentText.includes("Dog Licenses")) {
-    document.getElementById("currentAuthLevel-" + modalID).value =
-      currentText.replace("Dog Licenses,", "");
-  } else {
-    // add Dog Licenses to list.
-    document.getElementById("currentAuthLevel-" + modalID).value =
-      currentText + "Dog Licenses, ";
-  }
-}
-
-function policyAuth(modalID) {
-  // get current text in input field to append.
-  let currentText = document.getElementById(
-    "currentAuthLevel-" + modalID
-  ).value;
-
-  // check if currentText already contains 'Policies'. If it does, don't add it (return).
-  if (currentText.includes("Policies")) {
-    document.getElementById("currentAuthLevel-" + modalID).value =
-      currentText.replace("Policies,", "");
-  } else {
-    // add Policies to list.
-    document.getElementById("currentAuthLevel-" + modalID).value =
-      currentText + "Policies, ";
-  }
-}
-
 // display form based on selected option in <select>.
 function changeOptions(selectEl) {
   let selectedValue = selectEl.options[selectEl.selectedIndex].value;
