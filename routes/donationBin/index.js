fa35e022d0013931c1b1292b788a0cb8980eb1a6
Bobby <bobby.jonkman@hotmail.com>
2022-07-22 10:33:20 -0400

    renewal done, adding edit options

--- routes/donationBin/index.js
+++ routes/donationBin/index.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router();
 // models.
 var DonationBinCharity = require("../../models/donationBin/donationBinCharity");
+// express-validate.
+const { body, validationResult } = require("express-validator");
 // pagination lib.
 const paginate = require("express-paginate");
 
@@ -34,6 +36,49 @@ router.get("/", async (req, res, next) => {
       hasMorePages: paginate.hasNextPages(req)(pageCount),
     });
   });
+});
+
+// renew license.
+router.post("/", async (req, res, next) => {
+  // server side validation.
+  const errors = validationResult(req);
+
+  // if errors is NOT empty (if there are errors...).
+  if (!errors.isEmpty()) {
+    return res.render("donationBin/index", {
+      title: "BWG | Add Property Owner",
+      errorMessages: "Page Error!",
+      email: req.session.email,
+      auth: req.session.auth, // authorization.
+    });
+  } else {
+    // get current date for automatic population of license.
+    var issueDate = new Date();
+    // init expiryDate.
+    var expiryDate = new Date();
+
+    // if issueDate is in November or December.
+    if (issueDate.getMonth() === 10 || issueDate.getMonth() === 11) {
+      expiryDate = new Date(issueDate.getFullYear() + 2, 0, 31);
+    } else {
+      expiryDate = new Date(issueDate.getFullYear() + 1, 0, 31); // year, month (jan = 0), day
+    }
+
+    // update license.
+    DonationBinCharity.update(
+      {
+        issueDate: issueDate,
+        expiryDate: expiryDate,
+      },
+      {
+        where: {
+          donationBinCharityID: req.body.donationBinCharityID,
+        },
+      }
+    ).then(() => {
+      return res.redirect("/donationBin");
+    });
+  }
 });
 
 module.exports = router;
